7e0f8a0409b280fb7908f37df032b8407226aabc
eazypizycoders <hangout.abdullah@gmail.com>
2025-11-19 14:28:02 +0500

    First commit for Complete C++ Guide series

---
+++ Chapter 1/sytaxExplanation.cpp
@@ -0,0 +1,26 @@
+// Classical C++
+
+#include <iostream>
+// using namespace std;
+
+void cout()
+{
+    std::cout << "Inside the function";
+}
+
+int main()
+{
+    std::cout << "Hy this is our main program";
+
+    cout();
+    return 0;
+}
+
+// Modern C++
+
+// #include <iostream>
+// int main()
+// {
+//     std::cout<<"Hello";
+//     return 0;
+// }
