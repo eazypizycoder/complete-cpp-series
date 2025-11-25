6fd7db4730cf30d362a51764e68c0dd51f1d12d6
eazypizycoders <hangout.abdullah@gmail.com>
2025-11-25 17:50:41 +0500

    data types

---
+++ Chapter 2/dataTypes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+int main()
+{
+
+    /*
+    int 4 bytes =>32 bits => 1 byte= 8 bits 4*8=32 bits
+    double 8 bytes
+
+
+    */
+
+    // int age=21; //=> simple 21 => binary 01
+    // std::cout<<age;//0101 => decimal => 21
+
+    // std::cout<<std::endl;
+    // std::cout<<sizeof(age);
+
+    // bool (true false)
+    // double . point 1.2 2.3 =>8 bytes
+    // char 1 byte => a b c ab
+    // char a='c';
+    // std::cout<<sizeof(a);
+
+    // int a{12.1};
+    // int b=12;
+    // std::cout<<"The value of "<<a<<" and the value of b "<<b;
+    int a{};
+
+    std::cout << a;
+
+    // double age=12.1;
+    // double age2{12};
+    // std::cout<<age<<" "<<age2;
+
+    return 0;
+}
