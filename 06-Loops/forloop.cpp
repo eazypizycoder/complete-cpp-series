150c1aedd2ca6a0a35c4e83c2d300e34f7ffd1e3
eazypizycoder <collaborate.abdullah@gmail.com>
2025-12-28 13:00:24 +0500

    for loop

---
+++ 06-Loops/forloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+    // for loop
+
+    for (int i = 0; i < 1000; ++i)
+    {
+        std::cout << "The number of i is : "<<i << std::endl;
+    }
+
+
+    // cout<<1<
+    
+    return 0;
+}
