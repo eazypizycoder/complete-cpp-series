b15015274b080585d4521408879d3004edf094a0
eazypizycoder <collaborate.abdullah@gmail.com>
2025-12-29 12:08:23 +0500

    do while loop

---
+++ 06-Loops/dowhile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    // inili
+    int i = 10;
+
+    do
+    {
+        std::cout << i << std::endl;
+        ++i;
+        // loop body
+    } while (i < 10);
+    return 0;
+}
